85b2986a343d28d51c5b851dfcc6f13be18352e5
Hamza <boujahamza@gmail.com>
2022-05-20 11:00:11 +0100

    added login form controllers

--- src/app/home/components/login/login.component.ts
+++ src/app/home/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  
+  email = new FormControl('');
+  password = new FormControl('');
+  loginSuccess = true;
 
-  constructor() { }
+  constructor(private auth: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  public attemptLogin() {
+    let email = this.email.value;
+    let password = this.password.value;
+
+    this.auth.login(email, password).subscribe({
+      next: authResult => {
+        this.auth.setSession(authResult);
+        this.loginSuccess = true;
+        this.router.navigateByUrl('/');
+      },
+      error: error => {
+        this.loginSuccess = false;
+      }
+    });
   }
 
 }
