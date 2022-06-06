2f6d1a858d9388e15ffe779367cea18b96228dbf
Hamza <boujahamza@gmail.com>
2022-06-06 18:26:20 +0100

    added basic feed display

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Article } from '../cards/cards.component';
+import { AuthService } from 'src/app/services/auth.service';
+import { FeedService } from 'src/app/services/feed.service';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +9,52 @@ import { Article } from '../cards/cards.component';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
-  cards: Article [] = []
+  public feed: any[] = [];
 
-  ngOnInit(): void {
+  constructor(private feedService: FeedService, public auth: AuthService) { }
+
+  async ngOnInit() {
+    let rawFeed = await this.feedService.getFeed();
+    if(rawFeed){
+      let reviews = rawFeed.reviews;
+      let articles = rawFeed.articles;
+      let element;
+        while(articles && reviews && reviews.length != 0 && articles.length != 0) {
+          if(reviews[0].timestamp < articles[0].timestamp) {
+            element = articles.shift();
+            element.type = "article";
+            this.feed.push(element);
+          }else{
+            element = reviews.shift();
+            element.type = "review";
+            this.feed.push(element);
+          }
+        }
+      this.feed.push(...articles.map((element) => {
+        element.type = "article";
+        return element;
+      }));
+      this.feed.push(...reviews.map((element) => {
+        element.type = "review";
+        return element;
+      }));
+      
+      console.log(this.feed);
+    }
+  }
+
+  ratingStar(value:number,rating:number) {
+    if(value<=rating){
+      return("star");
+    }else if((value-rating)>0.2 && (value-rating)<1){
+      return("star_half");
+    }else{
+      return("star_border");
+    }
+  }
+
+  async getUsername(user_id:string){
+    let username = await this.auth.getUsernameFromId(user_id).toPromise();
+    return username?.username;
   }
 }
--- src/app/services/feed.service.ts
+++ src/app/services/feed.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+import { UserInfoService } from './user-info.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FeedService {
 
-  constructor() { }
+  private feedHost = "http://localhost:4000/feed";
+
+  constructor(private http: HttpClient, private auth: AuthService, private userInfoService: UserInfoService) { }
+
+
+  async getFeed() {
+    let feed: RawFeed | undefined;
+    let user_id = await this.auth.getUser_id();
+    if(user_id) {
+      let following = await this.userInfoService.getFollowing(user_id).toPromise();
+      feed = await this.http.post<RawFeed>(this.feedHost, {"following": following}).toPromise();
+    }
+    return feed;
+  }
+
+}
+
+interface RawFeed {
+  articles: Array<any>,
+  reviews: Array<any>,
 }
